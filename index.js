59df06f230c1b91f3aa646fdb349014b012dc6f9
Yeo <axel.kirac2000@gmail.com>
2022-11-22 10:38:42 +0100

    add new button for dir

--- index.js
+++ index.js
@@ -30,13 +30,16 @@ const buttonTest = require('./buttonbot/buttonTest.js')
 const cmdAna = require('./messagebot/cmdana.js')
 
 //Button Role PPA/CMAP /!\ Retirer le mode commentaire que pour envoyé le rôle
-const buttonRolePpa = require('./buttonbot/buttonRolePpa.js')
+// const buttonRolePpa = require('./buttonbot/buttonRolePpa.js')
 
 //Handle des buttons
 const handleButtonRolePpa = require('./buttonbot/buttonRolePpa.js')
 
 //Statut du bot
 const setStatus = require('./setstatus/setStatus.js')
+
+//Ticket Direction Setup
+const createButtonDirection = require('./directionTicket/ticket1.js')
 
 //--------------------------------Set Statut-----------------------------------------
 
@@ -53,7 +56,10 @@ cmdAna.cmdAna
 
 //Button
 buttonTest.buttonTest
-buttonRolePpa.buttonRolePpa
+// buttonRolePpa.buttonRolePpa
 handleButtonRolePpa.handleButtonRolePpa
+
+//Ticket Direction
+createButtonDirection.createButtonDirection
 
     
--- tokenclient/token.js
+++ tokenclient/token.js
@@ -1,1 +1,10 @@
-module.exports = "MTAxMDE5MTI3NzIzODc4NDA1MA.G9p3hB.lwkuJdehOMORRqAVe4xgVEquKaOI4M3whZcv1U"
+module.exports = "MTA0MjQxNjcxMTIwNDMzOTczMw.GYMkPb.66F9zFbYNi_NwbxNT8SoKPCa-DrdRjQOlelTM4"
+
+
+
+
+//Clé Ana Tomie
+//MTAxMDE5MTI3NzIzODc4NDA1MA.G9p3hB.lwkuJdehOMORRqAVe4xgVEquKaOI4M3whZcv1U
+
+//Clé Ana Thomas
+//MTA0MjQxNjcxMTIwNDMzOTczMw.GYMkPb.66F9zFbYNi_NwbxNT8SoKPCa-DrdRjQOlelTM4
