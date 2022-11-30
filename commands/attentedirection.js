50db8f685b2ecffa2aacad3ba68c2d972c891676
axel kirac <axel.kirac2000@gmail.com>
2022-11-30 16:25:23 +0100

    prepare for juan

--- commands/attentedirection.js
+++ commands/attentedirection.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('attentedirection')
+		.setName('attentedirectionb')
 		.setDescription('Envoi un message de réponse au Ticket Direction'),
 	async execute(interaction) {
 		await interaction.reply("Bonjour,\n\nUn membre <@&774368900527423548> ou un <@&774369294876016710> répondra bientôt à votre demande. Merci de vous assurer d'avoir correctement rédigée celle-ci. **Les demandes liées à des réanimations** ou **des absences à des rendez-vous d'Evaluations Psychotechniques** ne seront pas prises en compte et **le ticket sera fermé**. \n\n**Bonne journée,**\n<@1010191277238784050>\n**Secrétaire du L.S.M.C.<:LSMC:915255404076883988> **");
