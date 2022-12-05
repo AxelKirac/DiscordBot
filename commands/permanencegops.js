bd693f0f3da0a3e498248e5bb14461d24edd4927
Yeo <axel.kirac2000@gmail.com>
2022-12-05 11:41:49 +0100

    ajout des permanences gops fonctionnel

---
+++ commands/permanencegops.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, Client, GatewayIntentBits, Message } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('permanencegops')
+	.setDescription('Envoi une permanence GOPS')
+	.addStringOption(option =>
+			option.setName('début')
+			.setDescription('Indiqué quand la permanence commencera en respectant se schéma XXhXX')
+			.setRequired(true)
+			.setMinLength(5)
+			.setMaxLength(5))
+		.addStringOption(option =>
+            option.setName('fin')
+                .setDescription('Indiqué quand la permanence se terminera en respectant se schéma XXhXX')
+				.setRequired(true)
+				.setMinLength(5)
+				.setMaxLength(5)),
+				async execute(interaction) {
+					const responseStart = interaction.options.getString('début')
+					const responseEnd = interaction.options.getString('fin')
+					await interaction.reply(
+			{	allowedMentions: {role:['1006604363646644274']},
+				content : `Bonjour à tous et à toutes <@&1006604363646644274> !
+
+			Le docteur ${interaction.user} est actuellement disponible pour garder vos enfants à la garderie.
+			N'hésitez pas à prendre contact avec lui au 0523 par téléphone ou simple message !
+			
+			**Disponible de : ${responseStart} à ${responseEnd}**
+			
+			L'équipe du Gynecology Obstetrics & Pediatrics Services vous remercie pour votre confiance ! :GOPS: 
+										https://i.imgur.com/QxQPj9c.png`})
+	}
+};
+// *Numéro à remettre 774368900527423548
