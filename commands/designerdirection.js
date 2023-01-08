a39c74bef7758f4a41c70684be8d9ab684ab3a3c
Yeo <axel.kirac2000@gmail.com>
2023-01-08 01:52:40 +0100

    add pierre to permanence lo

---
+++ commands/designerdirection.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, Client, GatewayIntentBits, Message} = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('designerdirection')
+	.setDescription('Défini un Haut-Gradé ou Directeur qui doit répondre')
+	.addStringOption(option =>
+			option.setName('name')
+			.setDescription('Définir un haut gradé ou un directeur à ping (mentionner le avec le @ ici)')
+			.setRequired(true)
+			.setMinLength(1)),
+				async execute(interaction) {
+					const designerDirection = interaction.options.getString('name')
+					await interaction.reply(
+			{	allowedMentions: {users:true},
+			content : `Bonjour,\n\ ${designerDirection} répondra bientôt à votre demande. Merci de vous assurer d'avoir correctement rédigée celle-ci. **Les demandes liées à des réanimations** ou **des absences à des rendez-vous d'Evaluations Psychotechniques** ne seront pas prises en compte et **le ticket sera fermé**. \n\n**Bonne journée,**\n<@1010191277238784050>\n**Secrétaire du L.S.M.C.<:LSMC:915255404076883988> **` });
+	}
+};
+
+
+
+
+
---
+++ commands/permanencepierre.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, Client, GatewayIntentBits, Message } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('permanencepierre')
+	.setDescription('Envoi une permanence pour le Dr. Pierre Mic')
+	.addStringOption(option =>
+			option.setName('hopital')
+			.setDescription('Indiqué dans quel hôpital doit se déroulé la permanence (Pillbox ou Strawberry ?)')
+			.setRequired(true)
+			.setMinLength(1)
+			.setMaxLength(10))
+		.addStringOption(option =>
+            option.setName('endroit')
+                .setDescription('Ou allez vous être ? (Exemple : Couloir, Bureau)')
+				.setRequired(true)
+				.setMinLength(1)
+				.setMaxLength(15)),
+				async execute(interaction) {
+					const hospitalPierre = interaction.options.getString('hopital')
+					const endroitPierre = interaction.options.getString('endroit')
+					await interaction.reply(
+			{	allowedMentions: {role:['1006604363646644274']},
+				content : `
+Hey !
+Le docteur <@338709921698807809> est actuellement **disponible** à l'hôpital de **${hospitalPierre}** dans le **${endroitPierre}**!
+                
+**N'hésitez pas à prendre contact avec lui pour toute demande, récupérer votre prime ou réclamation !**
+https://cdn.discordapp.com/attachments/1004187230023204974/1008768195689205861/Gif_ritchy_fini.gif`})
+	}
+};
