c8593436b3a94574f5590e5372db15bd8e478e6c
Yeo <axel.kirac2000@gmail.com>
2023-01-16 10:02:36 +0100

    fix miss name

--- commands/reprejo.js
+++ commands/reprejo.js
@@ -7,6 +7,6 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply({ephemeral: true})
 		interaction.editReply("Demande bien prise en compte")
-		interaction.channel.send("Bonjour, \nLa **représentante du personnel** <@281868284955983883> est disponible à **l'hôpital Pillbox** ou par téléphone au **253348** afin de traiter vos problèmes en interne !\nN'hésitez pas à prendre contact avec elle pour vous confier !\n\n***Attention :*** Il ne s'agit pas d'une permanence pour les primes !\n\nhttps://cdn.discordapp.com/attachments/943141757586141195/1060594837239365683/gif_rp_jo.gif")
+		interaction.channel.send("Bonjour, \nLe **représentant du personnel** <@1064450257808789525> est disponible à **l'hôpital Pillbox** ou par téléphone au **253348** afin de traiter vos problèmes en interne !\nN'hésitez pas à prendre contact avec elle pour vous confier !\n\n***Attention :*** Il ne s'agit pas d'une permanence pour les primes !\n\nhttps://cdn.discordapp.com/attachments/943141757586141195/1060594837239365683/gif_rp_jo.gif")
 	},
 };
