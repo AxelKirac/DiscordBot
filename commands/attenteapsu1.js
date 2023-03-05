be12be3ef291e35fc18bea1bf22b0fb7073d2146
Yeo <axel.kirac2000@gmail.com>
2023-03-05 16:49:59 +0100

    ajout des commandes pour APSU modifié

--- commands/attenteapsu1.js
+++ commands/attenteapsu1.js
@@ -8,6 +8,6 @@ module.exports = {
 		await interaction.deferReply({ephemeral: true})
 		interaction.editReply("Demande bien prise en compte, attente d'un psychologue")
 						 interaction.channel.send({ allowedMentions: {roles :['864329971408961546']},
-						content: "Bonjour, \n\n Nous avons bien reçu votre demande de **chirurgie esthétique.** :APSU:1081959516846968914 Pour votre première consultation, un rendez vous avec un psychologie est obligatoire.\n Un membre du <@&864329971408961546> vas prendre contacte avec vous afin de vous proposer un rendez vous. \n Restant à disposition \n\n Bien à vous,\n<@1010191277238784050>\n**Secrétaire du L.S.M.C.<:LSMC:915255404076883988> **"});
+						content: "Bonjour, \n\n Nous avons bien reçu votre demande de **chirurgie esthétique.** :APSU:1081959516846968914 Pour votre première consultation, un rendez vous avec un psychologue est obligatoire.\n Un membre du <@&864329971408961546> vas prendre contacte avec vous afin de vous proposer un rendez vous. \n Restant à disposition \n\n Bien à vous,\n<@1010191277238784050>\n**Secrétaire du L.S.M.C.<:LSMC:915255404076883988> **"});
 	}
 };
