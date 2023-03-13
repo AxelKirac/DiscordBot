1ac5eddc734793db0196bc2dbddc75e2c7970094
Yeo <axel.kirac2000@gmail.com>
2023-03-13 16:42:11 +0100

    ajout de attenteapsu3

---
+++ commands/attenteapsu3.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, Client, GatewayIntentBits, Message} = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('attenteapsu3')
+	.setDescription('Défini le psychologue en charge du suivi')
+	.addStringOption(option =>
+			option.setName('name')
+			.setDescription('Définir le psychologue à ping (mentionner le avec le @ ici)')
+			.setRequired(true)
+			.setMinLength(1)),
+				async execute(interaction) {
+					await interaction.deferReply({ephemeral: true})
+					const designerPsy = interaction.options.getString('name')
+					interaction.editReply("Demande bien prise en compte")
+						 interaction.channel.send({
+						 content : `
+Il s'agit ici de votre troisième rendez-vous suite à une chirurgie esthétique.
+Celui-ci va par conséquent se dérouler avec un psychologue afin de s'assurer de votre bien être.
+                         
+Celui-ci et totalement obligatoire
+Ne vous inquiétez pas, le ${designerPsy} va prendre rapidement contact avec vous. <:PSS:935573301823172691> 
+N'hésitez pas à décrire au mieux votre ressenti, ce dernier est spécialisé dans cette branche, et saura répondre à vos questionnements.
+                         
+Restant à votre disposition pour toutes questions,
+                         
+Bien à vous,
+<@1010191277238784050>
+***Secrétaire du L.S.M.C. <:LSMC:915255404076883988>` });
+	}
+};
+
+
+
+
+
